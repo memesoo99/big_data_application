c552eace7d42c3be0b9ff695a0481714799f0e79
jisoo <genniferk1234@gmail.com>
2020-11-15 00:43:47 +0900

    ranking

--- customer_ranking.php
+++ customer_ranking.php
@@ -11,17 +11,13 @@ $conn = mysqli_connect($servername, $username, $password, "myDB");
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
-$sql = "SELECT * FROM customerinfo INNER JOIN storetype ON customerinfo.store_type = storetype.id";
-<<<<<<< HEAD
+//$sql = "SELECT * FROM customerinfo INNER JOIN storetype ON customerinfo.store_type = storetype.id";
 
-$sql = "SELECT store_type, AVG(age) FROM CustomerInfo GROUP BY store_type";
-=======
-$sql = "SELECT store_type, AVG(age) FROM CustomerInfo join StoreType s where CustomerInfo.store_type=s.store_type GROUP BY store_type";
->>>>>>> b5e0b637cb55add10a9a2de77ac3b264ac37fb48
+$sql = "SELECT storetype.store_type, AVG(customerinfo.age) FROM customerinfo INNER JOIN storetype ON customerinfo.store_type = storetype.id GROUP BY store_type";
 $res = mysqli_query($conn,$sql);
 
-while($row = mysqli_fetch_array($res)){
-    printf("store_type:%d, average age: %d\n",$row["store_type"], $row["AVG(age)"]);
+while($row = mysqli_fetch_array($res,MYSQLI_ASSOC)){
+    printf("store_type:%s, average age: %d\n",$row["store_type"], $row["AVG(customerinfo.age)"]);
 }
 
 mysqli_close($conn);
