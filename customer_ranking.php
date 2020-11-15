d94fd1135eace7c79446da0ff8c3a4d39539fab1
jisoo <genniferk1234@gmail.com>
2020-11-15 21:00:50 +0900

    advanced analysis 3개 더해야되는데 뭐하징

--- customer_ranking.php
+++ customer_ranking.php
@@ -31,7 +31,7 @@ $sql = "SELECT storetype.store_type, AVG(customerinfo.age) FROM customerinfo INN
 $res = mysqli_query($conn,$sql);
 
 while($row = mysqli_fetch_array($res,MYSQLI_ASSOC)){
-    printf("store_type:%s, average age: %d\n",$row["store_type"], $row["AVG(customerinfo.age)"]);
+    printf("store_type:%s, average age: %d<br>\n",$row["store_type"], $row["AVG(customerinfo.age)"]);
 }
 
 mysqli_close($conn);
@@ -46,9 +46,9 @@ mysqli_close($conn);
 </head>
 <body style="background-color: bisque;">
     <p><h3>연령별 선호 매장 순위 top3</h3></p>
-    <form action='' method="POST">\
+    <form action='' method="POST">
         <input type="hidden" name="new" value="1">
-        나이대 <br>
+            나이대 <br>
         <select name="age" size=5>
             <option value="1">10대미만</option>
             <option value="2">10대</option>
@@ -83,10 +83,10 @@ mysqli_close($conn);
     WHERE (age BETWEEN {$bottom} AND {$top}))sub1 GROUP BY sub1.store_type";
     $res = mysqli_query($conn,$sql);
     $i=1;
-    printf("<연령 %d ~ %d세가 가장 선호하는 휴게소 음식>\n",$bottom, $top);
+    printf("<연령 %d ~ %d세가 가장 선호하는 휴게소 음식><br>\n",$bottom, $top);
     while($row = mysqli_fetch_array($res)){
-
-      printf("%d순위 : %s\n",$i, $row["store_type"]);
+      
+      printf("%d순위 : %s<br>\n",$i, $row["store_type"]);
       $i = $i + 1;
 
   }
